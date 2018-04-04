f158914e0427c6f0e110b03b98a0106d0b254935
stKhaDgar <stmaktavish@gmail.com>
2018-04-04 22:16:39 +0300

    add function

--- History/Form1.cs
+++ History/Form1.cs
@@ -28,6 +28,10 @@ namespace History
                 string ournames = yourname + hername;
                 return ournames;
             }
+            int lol()
+            {
+                return 2;
+            }
         }
     }
 }
